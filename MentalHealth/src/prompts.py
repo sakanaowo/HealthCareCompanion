fa89707ebaf713fe9b2c9b142377916678b1dfd5
sakanaowo <idiotbugg@gmail.com>
2025-03-18 19:00:27 +0700

    thêm prompt mặc định

---
+++ MentalHealth/src/prompts.py
@@ -0,0 +1,6 @@
+from Documents_.Chap4.METADATAXtractor import CUSTORM_QUESTION_GEN_TMPL
+
+CUSTORM_QUESTION_GEN_TMPL="""\
+Dưới đây là nội dung của phần: {context_str}
+Hãy tóm tắt các chủ đề và thực thể chính của phần này.
+Tóm tắt: """
